850b7eb661146149aeb68d3adff4e35b355f1730
BearJ3rk <shadelessnight@gmail.com>
2026-08-22 19:21:54 -0700

    Bump PEC Board version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.example.myvoiceboard"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     buildFeatures { viewBinding = true }
